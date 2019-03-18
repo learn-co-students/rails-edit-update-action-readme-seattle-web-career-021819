8580ecb7d934d1ecfa9c6fa469e982034913e679
Reid Boudreau <rtboudreau@gmail.com>
2019-03-18 12:18:43 -0700

    Done.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,11 +4,22 @@ class ArticlesController < ApplicationController
   end
 
   def show
+
     @article = Article.find(params[:id])
   end
 
   def new
     @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(title: params[:article][:title], description: params[:article][:description])
+    redirect_to article_path(@article)
   end
 
   def create
